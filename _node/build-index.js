441ba033e1cd985ba1b24dd61919c398f6439116
TheGiddyLimit <TheGiddyLimit@5etools.com>
2018-11-18 15:11:02 +0000

    feat: index directories

--- _node/build-index.js
+++ _node/build-index.js
@@ -1,17 +1,91 @@
 "use strict";
 
 const fs = require("fs");
+const uf = require("./util-fs");
+const um = require("./util-misc");
 
-const DIR_COLLECTION = "collection";
-const FILE_INDEX = "index.json";
+function _ascSort (a, b) {
+	return b === a ? 0 : b < a ? 1 : -1;
+}
 
-const outIndex = {};
-fs.readdirSync(DIR_COLLECTION, "utf8")
-	.filter(file => file !== FILE_INDEX)
-	.forEach(file => {
-		const data = JSON.parse(fs.readFileSync(`${DIR_COLLECTION}/${file}`, "utf8"));
-		outIndex[file] = Object.keys(data).filter(it => !it.startsWith("_"));
+function buildCollectionIndex () {
+	um.info(`COLLECTIONS`, `Indexing...`);
+	const DIR_COLLECTION = "collection";
+	const FILE_INDEX = "index.json";
+
+	const outIndex = {};
+	fs.readdirSync(DIR_COLLECTION, "utf8")
+		.filter(file => file !== FILE_INDEX)
+		.forEach(file => {
+			const data = JSON.parse(fs.readFileSync(`${DIR_COLLECTION}/${file}`, "utf8"));
+			outIndex[file] = Object.keys(data).filter(it => !it.startsWith("_")).sort(_ascSort);
+		});
+
+	fs.writeFileSync(`${DIR_COLLECTION}/${FILE_INDEX}`, JSON.stringify(outIndex, null, "\t") + "\n");
+	um.info(`COLLECTIONS`, `Complete.`);
+}
+
+function buildTimestampIndex () {
+	um.info(`TIMESTAMPS`, `Indexing...`);
+	const RUN_TIMESTAMP = Math.floor(Date.now() / 1000);
+	const NO_META = {
+		"collection/index.json": 1
+	};
+	const TIMESTAMP_PATH = "_generated/index-timestamps.json";
+
+	const timestampIndex = {};
+
+	function indexDir (folder) {
+		const files = uf.listFiles(folder);
+		files
+			.map(file => ({
+				name: file,
+				contents: uf.readJSON(file)
+			}))
+			.forEach(file => {
+				const hasMeta = !NO_META[file.name];
+				if (!file.contents._meta && hasMeta) {
+					throw new Error(`File "${file.name}" did not have metadata!`);
+				}
+				if (hasMeta && file.contents._meta.dateAdded == null) {
+					um.warn(`TIMESTAMPS`, `\tFile "${file.name}" did not have "dateAdded"!`);
+					file.contents._meta.dateAdded = RUN_TIMESTAMP;
+				}
+				if (hasMeta) {
+					timestampIndex[file.name] = file.contents._meta.dateAdded;
+				}
+			});
+	}
+
+	uf.runOnDirs((dir) => {
+		um.info(`TIMESTAMPS`, `Indexing dir "${dir}"...`);
+		indexDir(dir);
 	});
-fs.writeFileSync(`${DIR_COLLECTION}/${FILE_INDEX}`, JSON.stringify(outIndex, null, "\t") + "\n");
 
-console.log("Indexing complete.");
+	um.info(`TIMESTAMPS`, `Saving timestamp index to ${TIMESTAMP_PATH}`);
+	fs.writeFileSync(`./${TIMESTAMP_PATH}`, JSON.stringify(timestampIndex), "utf-8");
+}
+
+function buildDirIndex () {
+	um.info(`DIRECTORY`, `Indexing...`);
+
+	uf.runOnDirs((dir) => {
+		console.log(`[DIRECTORY] Indexing dir "${dir}"...`);
+		const dirContent = fs.readdirSync(dir, "utf8")
+			.filter(file => file.endsWith(".json"))
+			.map(file => `${dir}/${file}`);
+
+		const dirFiles = dirContent.map(it => ({
+			download_url: `https://raw.githubusercontent.com/TheGiddyLimit/homebrew/master/feat/${encodeURIComponent(it)}`
+		}));
+
+		fs.writeFileSync(`_generated/index-dir-${dir}.json`, JSON.stringify(dirFiles), "utf-8");
+	});
+
+	um.info(`DIRECTORY`, `Complete.`);
+}
+
+buildCollectionIndex();
+buildTimestampIndex();
+buildDirIndex();
+um.info(`INDEX`, `Complete.`);
--- _node/clean-build.js
+++ _node/clean-build.js
@@ -3,8 +3,9 @@
 "use strict";
 
 const fs = require("fs");
+const uf = require("./util-fs");
+const um = require("./util-misc");
 
-const RUN_TIMESTAMP = Math.floor(Date.now() / 1000);
 const REPLACEMENTS = {
 	"—": "\\u2014",
 	"–": "\\u2013",
@@ -14,70 +15,23 @@ const REPLACEMENTS = {
 	"”": '\\"',
 	"…": "..."
 };
-const NO_META = {
-	"collection/index.json": 1
-};
-
-const TIMESTAMP_PATH = "_generated/index-timestamps.json";
-
-const timestampIndex = {};
-
-function isDirectory (path) {
-	return fs.lstatSync(path).isDirectory();
-}
-
-function readJSON (path) {
-	try {
-		return JSON.parse(fs.readFileSync(path, "utf8"));
-	} catch (e) {
-		e.message += ` (Path: ${path})`;
-		throw e;
-	}
-}
-
-function listFiles (dir) {
-	const dirContent = fs.readdirSync(dir, "utf8")
-		.filter(file => file.endsWith(".json"))
-		.map(file => `${dir}/${file}`);
-	return dirContent.reduce((acc, file) => {
-		if (isDirectory(file)) {
-			acc.push(...listFiles(file));
-		} else {
-			acc.push(file);
-		}
-		return acc;
-	}, [])
-}
 
 const replacementRegex = new RegExp(Object.keys(REPLACEMENTS).join("|"), 'g');
 
 function cleanFolder (folder) {
-	const files = listFiles(folder);
+	const files = uf.listFiles(folder);
 	files
 		.map(file => ({
 			name: file,
-			contents: readJSON(file)
+			contents: uf.readJSON(file)
 		}))
 		.map(file => {
-			const hasMeta = !NO_META[file.name];
-			if (!file.contents._meta && hasMeta) {
-				throw new Error(`File "${file.name}" did not have metadata!`);
-			}
-			if (hasMeta && file.contents._meta.dateAdded == null) {
-				console.warn(`\tFile "${file.name}" did not have "dateAdded", adding one...`);
-				file.contents._meta.dateAdded = RUN_TIMESTAMP;
-			}
-			if (hasMeta) {
-				timestampIndex[file.name] = file.contents._meta.dateAdded;
-			}
 			file.contents = JSON.stringify(file.contents, null, "\t") + "\n";
 			return file;
 		})
 		.map(file => {
-			console.log(`\t- "${file.name}"...`);
-			file.contents = file.contents.replace(replacementRegex, (match) => {
-				return REPLACEMENTS[match];
-			});
+			um.info(`CLEANER`, `\t- "${file.name}"...`);
+			file.contents = file.contents.replace(replacementRegex, (match) => REPLACEMENTS[match]);
 			return file;
 		})
 		.forEach(file => {
@@ -85,13 +39,9 @@ function cleanFolder (folder) {
 		});
 }
 
-fs.readdirSync(".", "utf8")
-	.filter(dir => isDirectory(dir) && !dir.startsWith(".") && !dir.startsWith("_") && dir !== "node_modules")
-	.forEach(it => {
-		console.log(`Cleaning dir "${it}"...`);
-		cleanFolder(it);
-	});
-console.log("Cleaning complete.");
+uf.runOnDirs((dir) => {
+	um.info(`CLEANER`, `Cleaning dir "${dir}"...`);
+	cleanFolder(dir);
+});
 
-console.log(`Saving timestamp index to ${TIMESTAMP_PATH}`);
-fs.writeFileSync(`./${TIMESTAMP_PATH}`, JSON.stringify(timestampIndex), "utf-8");
+um.info(`CLEANER`, "Cleaning complete.");
