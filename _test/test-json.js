688e99c55c7ff79092648f8cc1a8eabfa2c8e814
TheGiddyLimit <thegiddylimit@5etools.com>
2022-12-23 14:46:19 +0000

    refactor: migrate test/index scripts to dependency

--- _test/test-json.js
+++ _test/test-json.js
@@ -1,37 +1,3 @@
-import * as fs from "fs";
-import {Command} from "commander";
+import {BrewTester} from "5etools-utils";
 
-import {Um, JsonTester} from "5etools-utils";
-
-const LOG_TAG = "JSON";
-const _IS_FAIL_SLOW = !!process.env.FAIL_SLOW;
-
-const program = new Command()
-	.argument("[file]", "File to test")
-;
-
-program.parse(process.argv);
-
-
-async function main () {
-	const jsonTester = new JsonTester({isBrew: true, tagLog: LOG_TAG, fnGetSchemaId: () => "homebrew.json"});
-
-	let results;
-	if (program.args[0]) {
-		results = jsonTester.getFileErrors({filePath: program.args[0]});
-	} else {
-		results = await jsonTester.pGetErrorsOnDirsWorkers({isFailFast: !_IS_FAIL_SLOW})
-	}
-
-	const {errors, errorsFull} = results;
-
-	if (errors.length) {
-		if (!process.env.CI) fs.writeFileSync(`_test/test-json.error.log`, errorsFull.join("\n\n=====\n\n"));
-		console.error(`Schema test failed (${errors.length} failure${errors.length === 1 ? "" : "s"}).`);
-		process.exit(1);
-	}
-
-	if (!errors.length) Um.info(LOG_TAG, `Schema test passed.`);
-}
-
-main();
+BrewTester.pTestJson(process.argv);
